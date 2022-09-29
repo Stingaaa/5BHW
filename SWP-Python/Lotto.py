0beee3b74a02f2c1b23abdb940155e487f0366eb
Maximilian-Pumpfer <mpumpfer@tsn.at>
2022-09-29 13:14:45 +0200

    update

--- SWP-Python/Lotto.py
+++ SWP-Python/Lotto.py
@@ -1,14 +1,14 @@
 import random
 
-def initArr(minV, maxV):
+def initArr(mM):
     arrZiehung = []
-    for x in range(minV, maxV+1):
+    for x in range(mM[0], mM[1]+1):
         arrZiehung.append(x)
     return arrZiehung
         
-def initStatistics(minV, maxV):
+def initStatistics(mM):
     dictStatistics = {}
-    for x in range(minV, maxV+1):
+    for x in range(mM[0], mM[1]+1):
         dictStatistics[x] = 0
     return dictStatistics
         
@@ -22,11 +22,11 @@ def printZiehung(arr):
 def printStatistics(dict):
     print(dict)
         
-def ziehungen(minV, maxV, anz, arr, dict):
+def ziehungen(mM, anz, arr, dict):
     for i in range(anz):
         for x in range(1,7):
-            rand = random.randint(minV, maxV)
-            arr[len(arr)-x], arr[rand-minV-1] = arr[rand-minV-1], arr[len(arr)-x]
+            rand = random.randint(mM[0], mM[1])
+            arr[len(arr)-x], arr[rand-mM[0]-1] = arr[rand-mM[0]-1], arr[len(arr)-x]
             dict[rand] += 1
     if anz == 1:
         return arr 
@@ -34,12 +34,11 @@ def ziehungen(minV, maxV, anz, arr, dict):
 
 if __name__ == '__main__':
 
-    minVal = int(input("Minimum Value: "))
-    maxVal = int(input("Maximum Value: "))
+    minMax = int(input("Minimum Value: ")), int(input("Maximum Value: "))
     
     print("Einfache Ziehung:")
-    printZiehung(ziehungen(minVal, maxVal, 1, initArr(minVal, maxVal), initStatistics(minVal, maxVal)))
+    printZiehung(ziehungen(minMax, 1, initArr(minMax), initStatistics(minMax)))
 
     anzZiehungen = input("Wie oft soll gezogen werden: ")
     print(anzZiehungen + " Ziehungen:")
-    printStatistics(ziehungen(minVal, maxVal, int(anzZiehungen), initArr(minVal, maxVal), initStatistics(minVal, maxVal)))
+    printStatistics(ziehungen(minMax, int(anzZiehungen), initArr(minMax), initStatistics(minMax)))
